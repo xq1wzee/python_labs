cba7714b30cdadf5a6fa361a5462f3a603e43a3f
Polina <151847996+xq1wzee@users.noreply.github.com>
2025-09-14 17:20:25 +0300

    create 03

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -6,6 +6,6 @@ base = price * (1 - discount / 100)
 vat_amount = base * (vat / 100)
 total = base + vat_amount
 
-print(f"База после скидки: {base:.2f} Р")
-print(f"НДС:               {vat_amount:.2f} Р")
-print(f"Итого к оплате:    {total:.2f} Р")
+print(f"База после скидки: {base:.2f} ₽")
+print(f"НДС: {vat_amount:.2f} ₽")
+print(f"Итого к оплате: {total:.2f} ₽")
