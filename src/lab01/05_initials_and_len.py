4849f9753aa4e3debfb2c15e878deb1708ad2aa5
Polina <151847996+xq1wzee@users.noreply.github.com>
2025-09-25 12:32:21 +0300

    update

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,4 +1,6 @@
-name = input("ФИО: ")
+name = input("ФИО:").strip()
+name1 = name.split()
+name2 = ' '.join(name1)
 
-print(f"Инициалы: {''.join([i[0].upper() for i in name.split()])}.")
-print(f"Длина: {len(name.strip())}")
+print(f"Инициалы: {name1[0][0]}{name1[1][0]}{name1[2][0]}.")
+print(f"Длина: {len(name2)}")
