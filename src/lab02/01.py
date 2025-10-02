dfc2284f8b1d9b1a92c41e5e70ef9411f191c364
Polina <151847996+xq1wzee@users.noreply.github.com>
2025-10-02 12:54:20 +0300

    01upd

--- src/lab02/01.py
+++ src/lab02/01.py
@@ -1,8 +1,8 @@
 def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     if len(nums) == 0:
         return ValueError
-    mn = 9e6 
-    mx = -9e6 
+    mn = 9**100
+    mx = -(9**100)
     for i in range(len(nums)):
         if nums[i] < mn:
             mn = nums[i]
