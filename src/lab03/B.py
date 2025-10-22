050ea8adaf54c2d9be60ee5be4dd5d343d512c24
Polina <151847996+xq1wzee@users.noreply.github.com>
2025-10-22 15:21:39 +0300

    create B

---
+++ src/lab03/B.py
@@ -0,0 +1,13 @@
+import sys
+from src.lib.text import count_freq, top_n, normalize, tokenize
+
+text = sys.stdin.read()
+
+tokens = tokenize(text=normalize(text=text))
+top = top_n(count_freq(tokens))
+
+print(f"Всего слов: {len(tokens)}")
+print(f"Уникальных слов: {len(set(tokens))}")
+print("Топ-5:")
+for w, c in top:
+    print(f"{w}:{c}")
